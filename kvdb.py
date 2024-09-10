8b551fc6de79409cd17493fb1cca506f3d541c55
Ma, Guokai <guokai.ma@intel.com>
2024-09-10 17:00:58 +0800

    Copy transformer sink cache locally

--- kvdb.py
+++ kvdb.py
@@ -1,4 +1,5 @@
 from transformers import AutoTokenizer, AutoModelForCausalLM
+from cache import SinkCache
 from modeling_phi3 import Phi3ForCausalLM
 
 # 加载模型和分词器
@@ -7,14 +8,20 @@ tokenizer = AutoTokenizer.from_pretrained(model_name)
 model = Phi3ForCausalLM.from_pretrained(model_name, attn_implementation="eager")
 
 # 输入文本
-input_text = "Long long ago there was a little girl lived in a land far far away.  One day"
+input_text = "Continue the story with 500 words.  Long long ago there was a little girl lived in a land far far away.  One day"
 
-# 编码输入
-inputs = tokenizer(input_text, return_tensors="pt")
+cache = SinkCache(window_length=32, num_sink_tokens=4)
+length = 0
+new_text = ''
+
 
 # 生成文本
-outputs = model.generate(**inputs, max_length=50, num_return_sequences=1)
-
-# 解码并打印输出
-generated_text = tokenizer.decode(outputs[0], skip_special_tokens=True)
-print(generated_text)
+for i in range(256):
+    inputs = tokenizer(input_text, return_tensors="pt")
+    outputs = model.generate(**inputs, max_new_tokens=1, use_cache=True, num_return_sequences=1, past_key_values=cache)
+    generated_text = tokenizer.decode(outputs[0], skip_special_tokens=True)
+    new_text = generated_text[length:]
+    print(new_text, end='', flush=True)
+    length = len(generated_text)
+    #print(cache.get_seq_length())
+    input_text = generated_text
