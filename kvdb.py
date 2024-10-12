fb7332b35c967f591dfd13d5dfca2138d6c8e280
Ma, Guokai <guokai.ma@intel.com>
2024-10-12 11:17:01 +0800

    compute ppl instead of cross ppl

--- kvdb.py
+++ kvdb.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn.functional as F
 from transformers import AutoTokenizer, AutoModelForCausalLM
 from cache import PersistentCache
+from datasets import load_dataset
 from transformers.cache_utils import DynamicCache, SinkCache
 from modeling_phi3 import Phi3ForCausalLM
 from tqdm import tqdm
@@ -9,6 +10,8 @@ import argparse
 
 # define the following arguments:
 # debug -- print debug result, default false
+# full -- do a full perplexity computation
+# fast -- do a fast perplexity computation
 # ref -- test reference perplexity, default false
 # sink_size -- size of sink, defult 8
 # window_length -- length of window, default 32
@@ -18,6 +21,8 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument('--debug', action='store_true', default=False)
 parser.add_argument('--ref', action='store_true', default=False)
+parser.add_argument('--full', action='store_true', default=False)
+parser.add_argument('--fast', action='store_true', default=False)
 parser.add_argument('--sink_size', type=int, default=8)
 parser.add_argument('--window_length', type=int, default=32)
 parser.add_argument('--replace_sink_size', type=int, default=4)
@@ -75,7 +80,7 @@ def cross_perplexity3(input_text, model, tokenizer, cache=None):
     perplexity = torch.exp(loss)
     return perplexity.item()
 
-def cross_perplexity(input_text, model, tokenizer, cache=None):
+def perplexity(input_text, model, tokenizer, cache=None):
     # Compute average cross perplexity over each decode step
     inputs = tokenizer(input_text, return_tensors="pt")
     input_ids = inputs['input_ids']
@@ -108,47 +113,42 @@ model = Phi3ForCausalLM.from_pretrained(model_name, attn_implementation="eager")
 
 model_ref = AutoModelForCausalLM.from_pretrained(model_name, attn_implementation="eager")
 
+
 # Input text
 prompts = [
-          #"Earth, mostly harmless,",
           "Long long ago there was a little girl lived in a land far far away.  One day",
           ]
 
-text = "Long long ago there was a little girl lived in a land far far away.  One day she went to the forest to pick some flowers.  She saw a rabbit hopping around and she followed it.  The rabbit led her to a magical place where she met a fairy.  The fairy told her that she was the chosen one to save the kingdom from the evil witch."
-cache = PersistentCache(window_length=args.window_length, num_sink_tokens=args.sink_size, replace_sink_tokens=args.replace_sink_size)
-cache_ref = DynamicCache()
-ppl = cross_perplexity(text, model, tokenizer, cache)
+if args.fast:
+    text = "Long long ago there was a little girl lived in a land far far away.  One day she went to the forest to pick some flowers.  She saw a rabbit hopping around and she followed it.  The rabbit led her to a magical place where she met a fairy.  The fairy told her that she was the chosen one to save the kingdom from the evil witch."
+else:
+    # Load the WikiText dataset
+    dataset = load_dataset("wikitext", "wikitext-2-raw-v1", split="test")
+    text = '\n\n'.join(dataset["text"])
+    if not args.full:
+        text = text[:5000]
+if args.debug:
+    print(f'text={text}')
+
+if args.ref:
+    cache = DynamicCache()
+else:
+    cache = PersistentCache(window_length=args.window_length, num_sink_tokens=args.sink_size, replace_sink_tokens=args.replace_sink_size)
+ppl = perplexity(text, model, tokenizer, cache)
 
 '''
-total_ppl = 1
-total_ppl_ref = 1
-for prompt in prompts:
-    cache = PersistentCache(window_length=args.window_length, num_sink_tokens=args.sink_size, replace_sink_tokens=args.replace_sink_size)
-    if ref:
-        cache_ref = DynamicCache()
-    print('Generating text for test configuration')
-    result = gen_text(prompt, model, tokenizer, cache)
-    if ref:
-        print('Generating text for reference(default) configuration')
-        result_ref = gen_text(prompt, model_ref, tokenizer, cache_ref)
-    print('Compute cross perplexity for test configuration against reference configuration')
-    ppl = cross_perplexity(result, model, tokenizer, cache)
-    if ref:
-        print('Compute self perplexity for reference configuration against itself')
-        ppl_ref = cross_perplexity(result_ref, model_ref, tokenizer)
-    if debug:
+if args.debug:
+    total_ppl = 1
+    for prompt in prompts:
+        cache = PersistentCache(window_length=args.window_length, num_sink_tokens=args.sink_size, replace_sink_tokens=args.replace_sink_size)
+        print('Generating text for test configuration')
+        result = gen_text(prompt, model, tokenizer, cache)
+        print('Compute cross perplexity for test configuration against reference configuration')
+        ppl = cross_perplexity(result, model, tokenizer, cache)
         print(f'result={result}')
-        if ref:
-            print(f'result_ref={result_ref}')
         print(f'ppl={ppl}')
-        if ref:
-            print(f'ppl_ref={ppl_ref}')
-    total_ppl *= ppl
-    if ref:
-        total_ppl_ref *= ppl_ref
+        total_ppl *= ppl
 
-print(f'==============================================')
-print(f'geomean ppl = {total_ppl**(1/len(prompts))}')
-if ref:
-    print(f'geomean ppl_ref = {total_ppl_ref**(1/len(prompts))}')
+    print(f'==============================================')
+    print(f'geomean ppl = {total_ppl**(1/len(prompts))}')
 '''
